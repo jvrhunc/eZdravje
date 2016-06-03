91c14a9c40607128e8075bfd8cc9f8cbd26089c0
Jernej Vrhunc <jv4739@student.uni-lj.si>
2016-06-03 20:12:52 +0000

    dodana mapa

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -403,6 +403,14 @@ function dodajMeritveVitalnihZnakov() {
 	}
 }
 
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
 $(document).ready(function() {
     
     $('#preberiObstojeciEHR').change(function() {
