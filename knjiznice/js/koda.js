93ba3ba8ebcfd7f1c4d120c4dfe2850a0d3197f3
Jernej Vrhunc <jv4739@student.uni-lj.si>
2016-06-04 02:17:57 +0000

    apl. skor konc

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -274,6 +274,8 @@ function izvediMeritve(){
 		sessionId = getSessionId();
 		var ehrId = $("#preberiObstojeciEHR").val();
 		
+		$("#dodajVitalnoEHR").val(ehrId);
+		
 		var visine=["","","","","","","","","",""];
 		var teze=["","","","","","","","","",""];
 		var datumInUra =["","","","","","","","","",""];
@@ -293,7 +295,7 @@ function izvediMeritve(){
 					datumInUra[j] = datumInUra[j].substring(0,10);
 					//console.log(datumInUra[j]);
 				}
-				console.log(datumInUra);
+				//console.log(datumInUra);
 				
 				var result = ["","","","","",""];
 				var vmesneVisine = 0;
@@ -304,13 +306,34 @@ function izvediMeritve(){
 					result[k]= (vmesneTeze / (vmesneVisine*vmesneVisine))/10;
 					//console.log(result[k]);
 				}
+				
+				var rezultat = parseInt(result[0]);
+				//console.log("rezultat "+rezultat);
+			
+				if(rezultat < 18){
+					$("#sporociloBMI").html("<p class='zapar'>Kaj pa je s vami? Zakaj ste tako suhi? Ali veste, da po svetu vse več in več ljudi umira od podhranjenosti? Bi radi bili eden izmed njih? Resnično upam, da ne!</p>\
+											<p class='zapar'>Priporočam vam, da v tem trenutku obiščeč restavracijo, kjer se naužijete velike porcije ogljikovih hidratov(osebno najbolj priporočam špagete) in beljakovin!</p>\
+											<p class='zapar'>Prav tako vam priporočam da čimprej obiščete zdravnika, saj s podhranjenostjo ni šale! Zato hitro prste na tipkolnico in pojdite v zemljevid poiskat vam najbližjo restavracijo!</p>\
+											<p class='zapar'>Tukaj imate nekaj namigov kaj bi lahko iskali: restavracija, špagetarna, bolnica, zdravstveni dom, itd.</p>");
+				} else if (rezultat >= 18 && rezultat < 25){
+					$("#sporociloBMI").html("<p>Čestitam, vaša BMI vrednost je idealna, le tako naprej!</p>\
+											<p>Mislim da imam ravno pravšnjo idejo za vas..kaj ko bi obiskali kakšne terme, se relaksirali in proslavili vaše zdravo življenje?</p>\
+											 <p>Namigi za iskanje: terme, wellnes, kopališče, itd.</p>");
+				} else if (rezultat >=25 && rezultat < 30){
+					$("#sporociloBMI").html("<p>Kakor vsak dan pravim: <b>s trudom se daleč pride</b>. Le-tega boste vi še kako potrebovali, saj ste na zelo visoki ravni po BMI lestvici. Če se dovolj potrudite, mislim da uspete iz sebe spraviti par kilogramov, kajneda?</p>\
+											<p>Ne jejte več veliko mastne hrane, veliko telovadite in rezultat bo kmalu prišel tudi na vaš trebušček.</p>\
+											<p>Tukaj je še nekaj namigov za iskanje po vašem zemljevidu: trim road, fitnes center, šprtna dvorana, itd.</p>");
+				} else {
+					$("#sporociloBMI").html("<p>Kaj pa ste vi počeli skozi celotno življenje? Jedli pice? Pili koka-kolo? Namreč vaš BMI je izredno prevelik, kot vrjetno tudi vaš trebušček! Začnite skrbeti za sebe in ne pojejte vsega kar vidite!</p>\
+											<p>Obvezno obiščite zdravnika, če ne celo kar kliničnega centra!</p>\
+											<p>Nekaj napotkov za isaknje po zemljevidu: bolnica, bolnišnica, zdravstveni dom</p>");
+				}
+				
 				generirajGraf(result,datumInUra);
 			});
 		});
-
-	
-	
 }
+
 
 function generirajGraf(result,datumInUra){
 		var ctx = $("#myChart");
@@ -392,7 +415,7 @@ function dodajMeritveVitalnihZnakov() {
 		    data: JSON.stringify(podatki),
 		    success: function (res) {
 		        $("#dodajMeritveVitalnihZnakovSporocilo").html(
-              "<span class='obvestilo label label-success fade-in'>Uspešno dodane meritve za "+ehrId+" . Ponovno generiraj graf --></span>");
+              "<span class='obvestilo label label-success fade-in'>Uspešno dodane meritve za "+ehrId+" . Poizvedba--></span>");
 		    },
 		    error: function(err) {
 		    	$("#dodajMeritveVitalnihZnakovSporocilo").html(
@@ -403,12 +426,79 @@ function dodajMeritveVitalnihZnakov() {
 	}
 }
 
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 46.117427, lng: 14.836805},  
     zoom: 8
   });
+  var input = /** @type {!HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      autocomplete.setTypes(types);
+    });
+  }
+
+  setupClickListener('changetype-all', []);
+  setupClickListener('changetype-address', ['address']);
+  setupClickListener('changetype-establishment', ['establishment']);
+  setupClickListener('changetype-geocode', ['geocode']);
 }
 
 $(document).ready(function() {
@@ -416,16 +506,6 @@ $(document).ready(function() {
     $('#preberiObstojeciEHR').change(function() {
 		$("#preberiSporocilo").html("");
 		$("#preberiEHRid").val($(this).val());
-	});
-	
-	$('#preberiObstojeciVitalniZnak').change(function() {
-		$("#dodajMeritveVitalnihZnakovSporocilo").html("");
-		var podatki = $(this).val().split("|");
-		$("#dodajVitalnoEHR").val(podatki[0]);
-		$("#dodajVitalnoDatumInUra").val(podatki[1]);
-		$("#dodajVitalnoTelesnaVisina").val(podatki[2]);
-		$("#dodajVitalnoTelesnaTeza").val(podatki[3]);
-		$("#dodajVitalnoTelesnaTemperatura").val(podatki[4]);
 	});
 
 });
